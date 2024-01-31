382dc193087cdc376f7acc1e13590adabc96857c
HalidÜnal <halidunal@gmail.com>
2024-01-31 23:38:00 +0300

    auth.module configurations

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/users.schema';
+import { TokenSchema } from './schemas/tokens.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature(
+      [
+        {
+          name: "User",
+          schema: UserSchema
+        },
+        {
+          name: "Token",
+          schema: TokenSchema
+        },
+      ]
+    ),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET_KEY,
+        signOptions: {
+          expiresIn: process.env.JWT_EXPIRES_IN
+        }
+      })
+    })
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix("/api")
+  app.useGlobalPipes(new ValidationPipe)
+  
   const port = process.env.PORT || 3001
   await app.listen(port);
 }
