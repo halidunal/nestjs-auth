9950eff58766cb2f3735c3faf704bb73624cfc0c
HalidÜnal <halidunal@gmail.com>
2024-01-31 22:29:26 +0300

    app.module configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRoot("mongodb://localhost/nesths-auth")
+  ],
   controllers: [AppController],
   providers: [AppService, AuthService],
 })
